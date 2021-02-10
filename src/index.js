47d43ed59066183d627ef7a93096de48c1ea1c03
abed12345 <abedborsa1993@gmail.com>
2021-02-10 11:51:27 -0500

    turn squares into an empty array

--- src/index.js
+++ src/index.js
@@ -17,6 +17,10 @@ class Square extends React.Component {
   }
   
   class Board extends React.Component {
+    constructor(props){
+      super(props);
+      this.state = {squares : Array.fill(null)}
+    }
     renderSquare(i) {
       return <Square value={i} />;
     }
